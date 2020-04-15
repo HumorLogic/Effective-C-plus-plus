2bdcba319273f8eae68cdb60fff6827a1fc454e2
Humor <humor.ylw@outlook.com>
2020-04-15 22:40:32 +0800

    update

---
+++ Basics/Code/Function/funtemp.cpp
@@ -0,0 +1,46 @@
+// funtemp.cpp -- 使用函数模板
+
+#include<iostream>
+
+template <typename T>	// 或者 class T
+void Swap(T& a, T& b);
+
+
+// 把main_*() 改为main()再运行
+int main() {
+
+	using namespace std;
+
+	int wallet1 = 300;
+	int wallet2 = 500;
+
+	cout << "wallet1 = $" << wallet1;
+	cout << " wallet2 = $" << wallet2 << endl;
+	
+	Swap(wallet1, wallet2);
+	cout << "使用交换函数模板后:\n";
+	cout << "wallet1 = $" << wallet1;
+	cout << " wallet2 = $" << wallet2 << endl;
+
+	double price1 = 8.8;
+	double price2 = 66;
+	cout << "price1 = $" << price1;
+	cout << " price2 = $" << price2 << endl;
+
+	Swap(price1, price2);
+	cout << "使用交换函数模板后:\n";
+	cout << "price1 = $" << price1;
+	cout << " price2 = $" << price2 << endl;
+
+
+	return 0;
+}
+
+template <typename T>
+void Swap(T& a, T& b)
+{
+	T temp;
+	temp = a;
+	a = b;
+	b = temp;
+}
--- Basics/Code/Function/swaps.cpp
+++ Basics/Code/Function/swaps.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 
 void swapr(int& a, int& b);		//引用传递法
-
+void swapp(int* p, int* q);			//指针法
 
 
 // 把main_*() 改为main()再运行
-int main() {
+int main_swaps() {
 
 	using namespace std;
 
@@ -15,11 +15,18 @@ int main() {
 	cout << "wallet1 = $" << wallet1;
 	cout << " wallet2 = $" << wallet2 << endl;
 
+	cout << "使用引用交换内容:\n";
 	swapr(wallet1, wallet2);
 	cout << "wallet1 = $" << wallet1;
 	cout << " wallet2 = $" << wallet2 << endl;
 
+	cout << "使用指针交换内容:\n";
+	swapp(&wallet1, &wallet2);
 
+	cout << "wallet1 = $" << wallet1;
+	cout << " wallet2 = $" << wallet2 << endl;
+	
+	
 
 	return 0;
 }
@@ -30,4 +37,13 @@ void swapr(int& a, int& b)
 	temp = a;
 	a = b;
 	b = temp;
+}
+
+void swapp(int* p, int* q)
+{
+	int temp;
+	temp = *p;
+	*p = *q;
+	*q = temp;
+	
 }
