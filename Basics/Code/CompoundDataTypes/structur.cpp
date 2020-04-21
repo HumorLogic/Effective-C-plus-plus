a0918d4f203462dad79374a077d59c69f51cd0dd
Humor <humor.ylw@outlook.com>
2020-04-21 11:27:09 +0800

    struct

---
+++ Basics/Code/CompoundDataTypes/structur.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+struct inflatable
+{
+	char name[20];
+	float volume;
+	double price;
+};
+
+// 把main_*() 改为main()再运行
+int main() {
+
+	using namespace std;
+	inflatable guest = {
+		"Glorious Gloria",
+		1.88,
+		29.99
+	};
+
+	struct fruit {
+		float weight;
+		double price;
+	};
+
+	fruit apple;
+	apple = { 1.5,5.5 };
+
+	cout << "guest : " << guest.name << "\n";
+	cout << "apple total : " << apple.price*apple.weight << endl;
+
+
+
+	return 0;
+}
