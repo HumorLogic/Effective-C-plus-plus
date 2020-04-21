0881f3b49c08694e90eeb9b2d9406347353aa6c5
Humor <humor.ylw@outlook.com>
2020-04-21 18:31:12 +0800

    update

--- Basics/Code/CompoundDataTypes/enum_use.cpp
+++ Basics/Code/CompoundDataTypes/enum_use.cpp
@@ -1,9 +1,9 @@
 #include<iostream>
 
 enum Week{ Sun, Mon, Tue, Wed, Thu, Fri, Sat };
-
+enum direction { East = 11, West = 22, North = 33, South = 44 };
 // 把main_*() 改为main()再运行
-int main() {
+int main_enumuse() {
 
 	using namespace std;
 	Week today;
@@ -14,6 +14,9 @@ int main() {
 	cout << today << endl;
 	cout << day<< endl;
 	
+	direction dir;
+	dir = South;
+	cout << dir;
 
 	return 0;
 }
