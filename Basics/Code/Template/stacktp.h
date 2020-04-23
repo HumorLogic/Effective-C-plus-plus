92f190a603ef610ebc9a6b6e13fedb731140ac4e
Humor <humor.ylw@outlook.com>
2020-04-23 14:29:04 +0800

    class Template

---
+++ Basics/Code/Template/stacktp.h
@@ -0,0 +1,47 @@
+// stacktp.h -- stackÄ£°å
+#ifndef STACKTP_H
+#define STACKTP_H
+
+template <class T>
+class Stack 
+{
+private :
+	enum{MAX=10};
+	T items[MAX];
+	int top;
+public :
+	Stack();
+	bool isempty();
+	bool isfull();
+	bool push(const T& item);
+	bool pop(T& item);
+
+};
+
+
+template<class T>
+Stack<T>::Stack() {
+	top = 0;
+}
+
+template<class T>
+bool Stack<T>::isempty()
+{
+	return top == 0;
+}
+
+template <class T>
+bool Stack<T>::isfull() {
+
+	return top == MAX;
+}
+
+template<class T>
+bool Stack<T>::push(const T& item) {
+
+}
+
+template<class T>
+
+
+#endif // !STACKTP_H
